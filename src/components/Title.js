c633df40dcf4874889967d450c5f8432d20216bb
ankita007-coder <ankitabudhia42@gmail.com>
2023-07-19 18:10:48 +0530

    Components almost ready

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Title = ({title,subtitle}) => {
+  return <div>
+    <div className="section-title">
+        <h2>{title} <span>{subtitle}</span></h2>
+      </div>
+
+  </div>;
+};
+
+export default Title;
